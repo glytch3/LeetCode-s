5e16a6e5d46a1b23590d1e779feae078458d9072
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-08-24 21:10:26 +0530

    Time: 35 ms (49.24%), Space: 6 MB (12.98%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        //log n with base 3
-        //log n divided by log 3
+        /*log n with base 3
+        log n divided by log 3*/
         if(n<1)
         {
             return false;
