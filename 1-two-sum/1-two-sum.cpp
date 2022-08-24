f418c455ea1c7b22c3250cafb2129b1ac9c56a68
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-08-24 21:47:59 +0530

    Time: 1076 ms (5.15%), Space: 10.3 MB (75.73%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //bruteforce
         for(int i=0;i<nums.size();i++)
         {
             for(int j=0;j<nums.size();j++)
