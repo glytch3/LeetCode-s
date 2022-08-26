1e47ecc540e1d8c7294dde0e56b24b9a108818fe
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-08-27 02:28:44 +0530

    Time: 25 ms (35.34%), Space: 5.9 MB (67.33%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.c
+++ 9-palindrome-number/9-palindrome-number.c
@@ -3,26 +3,12 @@
 bool isPalindrome(int x){
         if(x<0)
             return false;
-        long a=0;
-        long b=x;
+        long a=0,b=x;
         while(b>0)
         {
             a=(a*10)+(b%10);
             b/=10;
         }
         
-        return (a==x);
-    
-     /*   
-    
-    
-    if (x < 0) return false;
-    long y=0;
-    long xx=x;
-    while (xx>0) {
-        y = y*10 + xx%10;
-        xx/=10;
-    }
-    return (y == x);
-*/
+        return (a==x);   //compare with original variable
 }
