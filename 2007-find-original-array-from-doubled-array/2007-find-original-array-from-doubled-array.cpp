0e70cd90726b2ba03dcc383db13882c1c68b874a
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-09-15 23:46:05 +0530

    Time: 702 ms (51.27%), Space: 146.5 MB (35.65%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        /*vector<int> res={};
+        vector<int> f={};
+        if(changed.size()%2==1)
+            return f;
+        int half=changed.size()/2;
+        sort(changed.begin(), changed.end());
+        for(int i=0;i<changed.size()/2;i++)
+        {
+            if(changed[i]*2!=changed[i+half])
+                return f;
+            res.push_back(changed[i]);
+        }
+        return res;*/
+        
+        if (size(changed) % 2) {
+            return {};
+        }
+        unordered_map<int, int> cnts;
+        for (const auto& x : changed) {
+            ++cnts[x];
+        }
+        vector<int> nums;
+        for (const auto& [x, _] : cnts) {
+            nums.emplace_back(x);
+        }
+        sort(begin(nums), end(nums));
+        vector<int> result;
+        for (const auto& x : nums) {
+            if (cnts[x] > cnts[2 * x]) {
+                return {};
+            }
+            for (int i = 0; i < cnts[x]; ++i, --cnts[2 * x]) {
+                result.emplace_back(x);
+            }
+        }
+        return result;
+    }
+};
