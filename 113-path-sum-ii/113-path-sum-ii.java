95ad2ac792408ea3f5dfa6cb9aeab65dae00c5a5
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-09-24 23:59:08 +0530

    Time: 2 ms (85.41%), Space: 44.3 MB (78.21%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -0,0 +1,24 @@
+public class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if(root == null) return res;
+        build(root, new ArrayList<Integer>(), 0, sum);
+        return res;
+    }
+
+    void build(TreeNode root, List<Integer> list, int cur, int target){
+        if(root.left == null && root.right == null){
+            if(cur + root.val == target){
+                list.add(root.val);
+                res.add(new ArrayList<>(list));
+                list.remove(list.size()-1);
+            }
+            return;
+        }
+        list.add(root.val);
+        if(root.left != null) build(root.left, list, cur+root.val, target);
+        if(root.right != null) build(root.right, list, cur + root.val, target);
+        list.remove(list.size()-1);
+    }
+}
