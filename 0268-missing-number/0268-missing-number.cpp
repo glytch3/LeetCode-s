69c0b861b5374f56c7218f974844e46fba597f69
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-10-14 00:11:19 +0530

    Time: 49 ms (30.55%), Space: 18.1 MB (20.40%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,7 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int check=0;
-        for(auto x:nums)
+        for(auto& x:nums)
         {
             if(x!=check)
                 return check;
