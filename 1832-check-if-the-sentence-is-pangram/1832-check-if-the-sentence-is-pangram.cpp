424b83ca4d691cc3196b4d8a2ca2f4a86a52bef3
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-10-17 19:10:19 +0530

    Time: 7 ms (38.35%), Space: 6.5 MB (55.40%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -10,11 +10,27 @@ public:
         {
             if(sentence[i]==a[p])
                 p++;
+        }
+        if(p==26)
+            return true;
+        return false;
+    }
+    
+    /*bool checkIfPangram(string sentence) {
+        sort(sentence.begin(),sentence.end());
+        string a="abcdefghijklmnopqrstuvwxyz";
+        int p=0;
+        if(sentence.size()<26)
+            return false;
+        for(int i=0;i<sentence.size();i++)
+        {
+            if(sentence[i]==a[p])
+                p++;
             if(sentence.size()-i<26-p)
                 return false;
         }
         if(p==26)
             return true;
         return false;
-    }
+    }*/
 };
