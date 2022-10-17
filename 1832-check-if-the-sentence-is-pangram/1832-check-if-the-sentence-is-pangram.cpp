8935d2fd7901e9a91258950107c0011d1ef82fda
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-10-17 19:07:47 +0530

    Time: 6 ms (47.48%), Space: 6.3 MB (98.63%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        sort(sentence.begin(),sentence.end());
+        string a="abcdefghijklmnopqrstuvwxyz";
+        int p=0;
+        if(sentence.size()<26)
+            return false;
+        for(int i=0;i<sentence.size();i++)
+        {
+            if(sentence[i]==a[p])
+                p++;
+        }
+        if(p==26)
+            return true;
+        return false;
+    }
+};
