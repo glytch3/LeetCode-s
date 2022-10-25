3f4f849f57639cb49a5b72101327249f99bb19fd
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-10-25 23:37:30 +0530

    Time: 17 ms (7.88%), Space: 11.4 MB (77.71%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-/*    string concat(vector<string>& word)
+
+    string concat(vector<string>& word)
     {
         string a="";
         for(auto i:word)
             a.append(i);
         return a;
     }
-*/
-    string concat(vector<string>& word)
+    
+/*  string concat(vector<string>& word)
     {
         for(int i=1;i<word.size();i++)
             word[0].append(word[i]);
         return word[0];
     }
+*/
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         if(concat(word1)==concat(word2))
             return true;
