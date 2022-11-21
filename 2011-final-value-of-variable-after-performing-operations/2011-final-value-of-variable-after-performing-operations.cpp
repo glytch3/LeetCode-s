d9598834865809a5c413b2fa37801822e8428088
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-11-21 19:56:52 +0530

    Time: 22 ms (36.93%), Space: 14.1 MB (9.91%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        for(auto a:operations)
+        {
+            if (a[1]=='+')
+            x=x+1;
+            else
+            x=x-1;  
+        }
+        return x;
+    }
+};
