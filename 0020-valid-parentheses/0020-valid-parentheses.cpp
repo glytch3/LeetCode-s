1ba9f6493e3030efcb56f12a213dc0d4b0494a1b
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-11-25 22:06:31 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++) {
+            char top = st.empty() ? '#' : st.top(); //this
+            if(top == '(' && s[i] == ')') {
+                st.pop();
+            }else if(top == '{' && s[i] == '}') {
+                st.pop();
+            }else if(top == '[' && s[i] == ']') {
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }    
+        }
+        return st.empty();
+    }
+};
