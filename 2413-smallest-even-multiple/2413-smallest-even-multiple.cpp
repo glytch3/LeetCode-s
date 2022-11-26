d6a063ab68aabed9038ce4fcbaeba137c254eae1
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-11-26 19:14:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.53%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        if(n%2==0)
+        if((n & 1) == 0)
            return n;
-        else return n*2;
+        return n*2;
     }
 };
