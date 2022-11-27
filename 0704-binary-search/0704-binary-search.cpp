fed60d58d8f5226f30370e9ac157e81dea335cb2
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-11-27 19:36:34 +0530

    Time: 133 ms (5.21%), Space: 27.5 MB (64.21%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int result = binarySearch(nums, 0, n - 1, target);
+        if(result>-1)
+            return result;
+        else
+            return -1;
+    }
+    
+    int binarySearch(vector<int>& nums, int l, int r, int x)
+    {
+    if(nums[0]==x)
+        return 0;
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+        if (nums[mid] == x)
+            return mid;
+        if (nums[mid] > x)
+            return binarySearch(nums, l, mid - 1, x);
+        return binarySearch(nums, mid + 1, r, x);
+    }
+    return -1;
+    }
+};
