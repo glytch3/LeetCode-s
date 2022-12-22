e1489e391b6caabb437cca0add56c76526a5845a
glytch3 <92800167+glytch3@users.noreply.github.com>
2022-12-22 21:13:30 +0530

    Time: 3 ms (96.45%), Space: 8.8 MB (78.38%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-    const int n = image.size();
+    int n = image.size();
     for (int i = 0; i < n; ++i)
       for (int j = 0; j < (n + 1) / 2; ++j) {
         const int temp = image[i][j];
-        image[i][j] = image[i][n - j - 1] ^ 1;
-        image[i][n - j - 1] = temp ^ 1;
+        image[i][j] = image[i][n - j - 1]^1;
+        image[i][n - j - 1] = temp^1;
       }
     return image;
     }
