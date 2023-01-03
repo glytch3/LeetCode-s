7d880040e2cde2bb7cedc9b524544feef718cdf2
glytch3 <92800167+glytch3@users.noreply.github.com>
2023-01-03 23:42:51 +0530

    Time: 100 ms (45.34%), Space: 12 MB (100.00%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int n=strs[0].size();
+        //int n=strs[0].size();
         int m=strs.size();
         int ans=0;
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<(strs[0].size());i++)
             for(int j=0;j<m-1;j++)
             {
                 if(strs[j][i] > strs[j + 1][i]) 
