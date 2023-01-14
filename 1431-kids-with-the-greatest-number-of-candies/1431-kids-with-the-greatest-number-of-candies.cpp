2c982b1287acf2dc6d5d477a8703ed62937efa50
glytch3 <92800167+glytch3@users.noreply.github.com>
2023-01-14 22:36:27 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (61.07%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -5,11 +5,12 @@ public:
         int max=*max_element(candies.begin(), candies.end());
         for(auto x:candies)
         {
-            ans.push_back((x+extraCandies)>=max);
-            // if((x+extraCandies)<max)
-            //     ans.push_back(false);
-            // else
-            //     ans.push_back(true);
+            // ans.push_back((x+extraCandies)>=max);
+            
+            if((x+extraCandies)<max)
+                ans.push_back(false);
+            else
+                ans.push_back(true);
         }
         return ans;
     }
