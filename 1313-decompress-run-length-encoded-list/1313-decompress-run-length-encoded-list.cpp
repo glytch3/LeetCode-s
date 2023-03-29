bdc8944a828e24d11e7973193b842d1ab96e37b6
glytch3 <92800167+glytch3@users.noreply.github.com>
2023-03-29 23:59:04 +0530

    Time: 3 ms (91.42%), Space: 10.2 MB (10.54%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans;
         for(int i=0;i<nums.size();i=i+2)
         {
-            ans.insert(end(ans), nums[i], nums[i + 1]);
+            ans.insert(ans.end(),nums[i],nums[i+1]); // position , value , no. of values
         }
         return ans;
     }
