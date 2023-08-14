f8c072b26bf4e11e15eecbf2895e13033c766b9e
Glytch <afnan.althaf@gmail.com>
2023-08-14 16:25:06 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (30.87%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -24,7 +24,29 @@ public:
             return !ob[0][0];
         if(ob[m-1][n-1]==1  || ob[0][0]==1)
             return 0;
+        
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,ob,dp);
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                    dp[i][j]=1;
+                else if(ob[i][j]==1)
+                {
+                    dp[i][j]=0;
+                }
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
